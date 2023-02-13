3d04905a0d1910520a2fca7b7d8d7d8b9c146a06
Maher Abbass <mna73@mail.aub.edu>
2023-02-13 18:31:27 +0200

    Starting with the header

--- src/pages/Maher.jsx
+++ src/pages/Maher.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import "../assets/styles/header.css";
+import Logo from "../assets/images/logo.png";
 
 const Maher = () => {
   return (
-    <div className='Header'>Work in progress
+    <div className="header">
+      <img className="logo" src={Logo} alt="play store logo" />
+
+      <div class="navbar">
+        <ul>
+          <li>
+            <a href="#Games">Games</a>
+          </li>
+          <li>
+            <a href="#Apps">Apps</a>
+          </li>
+          <li>
+            <a href="#Movies">Movies & TV</a>
+          </li>
+          <li>
+            <a href="#Books">Books</a>
+          </li>
+        </ul>
+      </div>
+
+      <form action="" class="search-bar">
+        <input type="search" name="search" pattern=".*\S.*" required />
+        <button class="search-btn" type="submit">
+          <span>Search</span>
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default Maher;
